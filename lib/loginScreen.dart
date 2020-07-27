2a7f92cbfb59ce92abf307a847f6554aa633e25f
mpa19 <marcperezarnaiz@gmail.com>
2020-07-27 14:46:14 +0200

    Added dropDownButton and fixed UI login

--- lib/loginScreen.dart
+++ lib/loginScreen.dart
@@ -8,26 +8,75 @@ class loginScreen extends StatefulWidget {
 }
 
 class _loginScreenState  extends State<loginScreen>{
+  String dropdownValue = 'Marc';
+
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      body: Center(
-          child: ButtonTheme(
-              minWidth: 200,
-              buttonColor: Color(0xFF031e39),
-              child: FlatButton(
-                color: Colors.blueGrey, //Color(0xFF81A483),
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(
-                      builder: (context) => MyApp()
-                  ),
-                  );
-                },
-                child: Text('Login',style: TextStyle(color: Colors.white),
-                ),
+      body: Column(
+
+          children: <Widget>[
+
+            Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    DropdownButton<String>(
+                      value: dropdownValue,
+                      icon: Icon(Icons.arrow_downward),
+                      iconSize: 24,
+                      elevation: 16,
+                      style: TextStyle(
+                          color: Colors.deepPurple
+                      ),
+                      underline: Container(
+                        height: 2,
+                        color: Colors.deepPurpleAccent,
+                      ),
+                      onChanged: (String newValue) {
+                        setState(() {
+                          dropdownValue = newValue;
+                        });
+                      },
+                      items: <String>['Marc', 'Mario', 'Mike', 'Gerard']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      })
+                          .toList(),
+                    )
+                  ],
+                )
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  ButtonTheme(
+                    minWidth: 200,
+                    buttonColor: Color(0xFF031e39),
+                    child: FlatButton(
+                      color: Colors.blueGrey, //Color(0xFF81A483),
+                      onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(
+                            builder: (context) => MyApp()
+                        ),
+                        );
+                      },
+                      child: Text('Login',style: TextStyle(color: Colors.white),
+                      ),
+                    )
+                  )
+                ],
               )
-          )
+            ),
+        ]
       ),
     );
   }
